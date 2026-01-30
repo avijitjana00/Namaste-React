1fdfdbb846de51f700266c54c038d2495aab9c5b
avijit <avijitjana@gmail.com>
2026-01-30 16:53:36 +0530

    Episode-02 install react, react-dom, parcel

--- App.js
+++ App.js
@@ -1,42 +1,17 @@
-
-/**
- * 
- * <div id = "parent"> 
- *   <div id = "child1">
- *     <h1> I am h1 tag </h1>
- *     <h2> I am h2 tag </h2>
- *  </div>
- * <div id = "child2">
- *     <h1> I am h1 tag </h1>
- *     <h2> I am h2 tag </h2>
- *  </div>
- * </div>
- * 
- * 
- * 
- * 
- * 
-*/
-
-/**
- * Note: 
- * 1.React.createElement() method is used to create React elements and it takes three arguments:
- * 2.React.createElement() will return a react element object which is then rendered to the DOM using ReactDOM.
- * 3.ReactDOM is used to render React elements to the DOM.
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child1" },
         [
-            React.createElement("h1", {}, "I am h1 tab"),
-            React.createElement("h2", {}, "I am h2 tab")
+            React.createElement("h1", { key: "child1-h1" }, "I am h1 tab"),
+            React.createElement("h2", { key: "child1-h2" }, "I am h2 tab")
         ]
     ),
     React.createElement("div", { id: "child2" },
         [
-            React.createElement("h1", {}, "I am h1 tab"),
-            React.createElement("h2", {}, "I am h2 tab")
+            React.createElement("h1", { key: "child2-h1" }, "I am h1 tab"),
+            React.createElement("h2", { key: "child2-h2" }, "I am h2 tab")
         ]
     )
 );
--- READEME.md
+++ READEME.md
@@ -1,7 +1,11 @@
-/**
- * Note: 
- * 1.React.createElement() method is used to create React elements and it takes three arguments:
- * 2.React.createElement() will return a react element object which is then rendered to the DOM using ReactDOM.
- * 3.ReactDOM is used to render React elements to the DOM.
- * 
- */
+
+## Note: episode - 01
+ 1. React.createElement() method is used to create React elements and it takes three arguments:
+ 2. React.createElement() will return a react element object which is then rendered to the DOM using ReactDOM.
+ 3. ReactDOM is used to render React elements to the DOM.
+ 
+## Note: episode - 02
+ 1. install react
+ 2. install react-dom
+ 3. install parcel - as a bundler
+ 4. parcel run command - npx parcel "sourcefile.extenstion"
